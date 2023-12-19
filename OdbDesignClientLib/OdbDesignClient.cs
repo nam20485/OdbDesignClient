26fd526446f81f6196ed7249e6431cae60e30495
Nathan Miller <nmiller217@gmail.com>
2023-12-19 08:49:03 -0800

    implement basic HTTP client to fetch FileArchive as JSON from REST API

---
+++ OdbDesignClientLib/OdbDesignClient.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text.Json;
+//using System.Js
+
+namespace Odb.Client.Lib
+{
+    public class OdbDesignClient : HttpClient
+    {
+        public OdbDesignClient(Uri apiUri)
+        {
+            BaseAddress = apiUri;
+        }
+
+        public async Task<FileArchive> FetchFileArchiveAsync(string name)
+        {
+            FileArchive fileArchive = null;
+            var response = await GetAsync($"filemodel/{name}");
+            if (response.IsSuccessStatusCode)
+            {
+                var stream = await response.Content.ReadAsStreamAsync();
+                fileArchive = await JsonSerializer.DeserializeAsync<FileArchive>(stream, new JsonSerializerOptions()
+                {
+                    PropertyNameCaseInsensitive = true,                    
+                });                
+            }
+            return fileArchive;
+        }
+
+        public FileArchive FetchFileArchive(string name)
+        {
+            return FetchFileArchiveAsync(name).GetAwaiter().GetResult();
+        }
+    }
+}
